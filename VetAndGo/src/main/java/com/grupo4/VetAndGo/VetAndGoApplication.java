a8a7bc67df6bb12f7920c0a6d3f078907e9f4700
alvaro <alvaro@micorreo.com>
2025-10-23 09:00:18 +0200

    Añadido proyecto spring

---
+++ VetAndGo/src/main/java/com/grupo4/VetAndGo/VetAndGoApplication.java
@@ -0,0 +1,13 @@
+package com.grupo4.VetAndGo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VetAndGoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VetAndGoApplication.class, args);
+	}
+
+}
