080935e2f3efcbaad9678275140cd74ff26f876b
OmarchyDaniel <gilgomezdaniel71@gmail.com>
2026-01-26 11:44:03 +0100

    m

---
+++ VetAndGo/src/main/java/com/grupo4/VetAndGo/domain/infrastructura/CorsConfig.java
@@ -0,0 +1,18 @@
+package com.grupo4.VetAndGo.domain.infrastructura;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/api/**")
+        .allowedOrigins("http://vetandgo-bank-front.preproducciondaw.cip.fpmislata.com")
+        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+        .allowedHeaders("*")
+        .allowCredentials(true);
+  }
+}
