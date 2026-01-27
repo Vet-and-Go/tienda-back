1d6f9ca903c0d976b47b1535fdadcf03a0850841
OmarchyDaniel <gilgomezdaniel71@gmail.com>
2026-01-27 12:59:44 +0100

    Automatic Push from Omarchy

--- VetAndGo/src/main/java/com/grupo4/VetAndGo/domain/infrastructura/CorsConfig.java
+++ VetAndGo/src/main/java/com/grupo4/VetAndGo/domain/infrastructura/CorsConfig.java
@@ -9,8 +9,8 @@ public class CorsConfig implements WebMvcConfigurer {
 
   @Override
   public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping("/api/**")
-        .allowedOrigins("http://vetandgo-bank-back.preproducciondaw.cip.fpmislata.com")
+    registry.addMapping("*")
+        .allowedOrigins("*")
         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
         .allowedHeaders("*")
         .allowCredentials(true);
