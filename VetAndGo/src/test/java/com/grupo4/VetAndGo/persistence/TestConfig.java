3831b83aff88d895d35d29728f30d34cc2684598
OmarchyDaniel <gilgomezdaniel71@gmail.com>
2026-01-27 12:52:03 +0100

    updated CORS

--- VetAndGo/src/test/java/com/grupo4/VetAndGo/persistence/TestConfig.java
+++ VetAndGo/src/test/java/com/grupo4/VetAndGo/persistence/TestConfig.java
@@ -14,18 +14,19 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories(basePackages = "com.grupo4.VetAndGo.persistence.dao.jpa")
 @EntityScan(basePackages = "com.grupo4.VetAndGo.persistence.dao.jpa.entity")
 public class TestConfig {
-    @Bean
-    public CategoryJpaDao categoriaJpaDao(EntityManager entityManager) {
-        return new CategoryJpaDaoImpl();
-    }
+  @Bean
+  public CategoryJpaDao categoriaJpaDao(EntityManager entityManager) {
+    return new CategoryJpaDaoImpl();
+  }
+  /*
+   * @Bean
+   * public ProductJpaDao productJpaDao(EntityManager entityManager) {
+   * return new ProductJpaDaoImpl();
+   * }
+   */
 
-    @Bean
-    public ProductJpaDao productJpaDao(EntityManager entityManager) {
-        return new ProductJpaDaoImpl();
-    }
-
-    @Bean
-    public com.grupo4.VetAndGo.persistence.dao.jpa.UserJpaDao userJpaDao() {
-        return new com.grupo4.VetAndGo.persistence.dao.jpa.impl.UserJpaDaoImpl();
-    }
+  @Bean
+  public com.grupo4.VetAndGo.persistence.dao.jpa.UserJpaDao userJpaDao() {
+    return new com.grupo4.VetAndGo.persistence.dao.jpa.impl.UserJpaDaoImpl();
+  }
 }
