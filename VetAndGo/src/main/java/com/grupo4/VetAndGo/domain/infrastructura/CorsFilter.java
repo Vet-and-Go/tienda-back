a39385fc663d8ffd25394d6dbebb5e1159ee6f95
OmarchyDaniel <gilgomezdaniel71@gmail.com>
2026-01-28 12:43:35 +0100

    more

--- VetAndGo/src/main/java/com/grupo4/VetAndGo/domain/infrastructura/CorsFilter.java
+++ VetAndGo/src/main/java/com/grupo4/VetAndGo/domain/infrastructura/CorsFilter.java
@@ -3,11 +3,14 @@ package com.grupo4.VetAndGo.domain.infrastructura;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
 @Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class CorsFilter implements Filter {
 
   @Override
@@ -17,8 +20,8 @@ public class CorsFilter implements Filter {
     HttpServletResponse httpResponse = (HttpServletResponse) response;
     
     httpResponse.setHeader("Access-Control-Allow-Origin", "*");
-    httpResponse.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    httpResponse.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
+    httpResponse.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, PATCH");
+    httpResponse.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With, Accept");
     httpResponse.setHeader("Access-Control-Max-Age", "3600");
 
     if ("OPTIONS".equalsIgnoreCase(httpRequest.getMethod())) {
