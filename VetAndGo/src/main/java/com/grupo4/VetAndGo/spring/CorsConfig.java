fd2b768f0286b48e644166887a18ae8fbe519b18
OmarchyDaniel <gilgomezdaniel71@gmail.com>
2026-01-28 09:58:36 +0100

    yo que se

--- VetAndGo/src/main/java/com/grupo4/VetAndGo/spring/CorsConfig.java
+++ VetAndGo/src/main/java/com/grupo4/VetAndGo/spring/CorsConfig.java
@@ -21,19 +21,4 @@ public class CorsConfig implements WebMvcConfigurer {
         .allowedHeaders("*")
         .allowCredentials(false);
   }
-
-  @Bean
-  public CorsConfigurationSource corsConfigurationSource() {
-    CorsConfiguration config = new CorsConfiguration();
-    config.setAllowedOrigins(List.of(
-        "http://vetandgo-store-front.preproducciondaw.cip.fpmislata.com"));
-    config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-    config.setAllowedHeaders(List.of("*"));
-    config.setAllowCredentials(true);
-
-    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-    source.registerCorsConfiguration("/**", config);
-    return (CorsConfigurationSource) source;
-  }
-
 }
