cf2a5e42787c3481a4a1bf2375a5c83f5aa48078
OmarchyDaniel <gilgomezdaniel71@gmail.com>
2026-02-02 12:21:40 +0100

    Automatic Push from Omarchy

--- VetAndGo/src/main/java/com/grupo4/VetAndGo/domain/infrastructura/CorsConfig.java
+++ VetAndGo/src/main/java/com/grupo4/VetAndGo/domain/infrastructura/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig implements WebMvcConfigurer {
             "http://localhost:4201",
             "http://localhost:4202")
         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH", "HEAD")
-        .allowedHeaders("*")
+        .allowedHeaders("Content-Type", "Authorization", "X-Requested-With", "Accept")
         .allowCredentials(true)
         .maxAge(3600);
   }
