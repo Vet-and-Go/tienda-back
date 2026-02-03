a49e4ebddf455c49de40041e9447cab89509ec83
OmarchyDaniel <gilgomezdaniel71@gmail.com>
2026-02-03 20:54:13 +0100

    CorsConfig is THE SAME as deploy

--- VetAndGo/src/main/java/com/grupo4/VetAndGo/domain/infrastructura/CorsConfig.java
+++ VetAndGo/src/main/java/com/grupo4/VetAndGo/domain/infrastructura/CorsConfig.java
@@ -11,10 +11,11 @@ public class CorsConfig implements WebMvcConfigurer {
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
         .allowedOrigins(
-            "http://localhost:4202",
             "http://localhost:4200",
-            "http://localhost:8082",
-            "http://localhost:4201")
+            "http://localhost:4201",
+            "http://localhost:4202",
+            "http://vetandgo-bank-front.preproducciondaw.cip.fpmislata.com",
+            "http://vetandgo-store-front.preproducciondaw.cip.fpmislata.com")
         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH", "HEAD")
         .allowedHeaders("Content-Type", "Authorization", "X-Requested-With", "Accept")
         .allowCredentials(true)
