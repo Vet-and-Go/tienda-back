29e2aeafc032ed6ffd65fd4e7294cc15a6460b9d
OmarchyDaniel <gilgomezdaniel71@gmail.com>
2026-02-03 22:21:43 +0100

    Added allowedOriginPatters

--- VetAndGo/src/main/java/com/grupo4/VetAndGo/domain/infrastructura/CorsConfig.java
+++ VetAndGo/src/main/java/com/grupo4/VetAndGo/domain/infrastructura/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
-        .allowedOrigins(
+        .allowedOriginPatterns(
             "http://localhost:4200",
             "http://localhost:4201",
             "http://localhost:4202")
