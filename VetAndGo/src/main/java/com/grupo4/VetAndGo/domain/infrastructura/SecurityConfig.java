068f0115c06eedf363d5466dffb286705bdda27f
OmarchyDaniel <gilgomezdaniel71@gmail.com>
2026-02-03 22:32:03 +0100

    also in security

--- VetAndGo/src/main/java/com/grupo4/VetAndGo/domain/infrastructura/SecurityConfig.java
+++ VetAndGo/src/main/java/com/grupo4/VetAndGo/domain/infrastructura/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig {
   @Bean
   public CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration configuration = new CorsConfiguration();
-    configuration.setAllowedOrigins(Arrays.asList(
+    configuration.setAllowedOriginPatterns(Arrays.asList(
         "http://localhost:3000",
         "http://localhost:4200",
         "http://localhost:4201",
